c68df5e68ef1640208b25740642a7b47140ac936
thefirsthuman <Scipio172316>
2018-06-22 10:22:17 -0700

    Updated Binary Search exercise with a more efficient implementation.

--- binary-search/src/main/java/BinarySearch.java
+++ binary-search/src/main/java/BinarySearch.java
@@ -13,50 +13,29 @@ class BinarySearch<T extends Comparable<T>> {
     }
 
     /**
-     * Searches the list for the item T given as an argument in log n time
+     * Searches the list for the item T given as an argument in O(log n) time
      * @param item - The item to search for in the collection
      * @return Returns the index of the item given as an argument
      */
     int indexOf(T item) {
 
-        if (!itemList.isEmpty()) {
+        int lowerBound = 0;
+        int upperBound = itemList.size() - 1;
 
-            if (itemList.size() == 1) {
-                return 0;
+        while (lowerBound <= upperBound) {
+            int index = lowerBound + (upperBound - lowerBound) / 2;
+            int compareResult = item.compareTo(itemList.get(index));
+
+            if (compareResult == 0) {
+                return index;
             }
-
-            int lowerBound = 0;
-            int upperBound = itemList.size() - 1;
-            int index = (int) Math.ceil(itemList.size() / 2);
-
-            while (!isNotInArray(item, index, lowerBound, upperBound)) {
-                int compareResult = item.compareTo(itemList.get(index));
-
-                if (compareResult == 0) {
-                    return index;
-                }
-                else if (compareResult > 0) {
-                    lowerBound = index;
-                    index = (int)(Math.ceil(((double)index + upperBound) / 2));
-                }
-                else {
-                    upperBound = index;
-                    index = (int)(Math.floor(((double)index - lowerBound) / 2));
-                }
+            else if (compareResult > 0) {
+                lowerBound = index + 1;
+            }
+            else {
+                upperBound = index - 1;
             }
         }
         return -1;
-    }
-
-    /**
-     * Evaluates cases in which the item does not exist in the collection
-     * @param item - The item to find
-     * @param index - The current index
-     * @param lowerBound - The lower bound of the search space
-     * @param upperBound - The upper bound of the search space
-     * @return Returns true if the item cannot be found in the collection
-     */
-    private boolean isNotInArray(T item, int index, int lowerBound, int upperBound) {
-        return (lowerBound == upperBound) || ((index == upperBound || index == lowerBound) && item != itemList.get(index));
     }
 }
